4b770b82bbe2227e8dba2916d88ef94a291384f4
Jim Paulo Ovejera <f360.jovejera@gmail.com>
2015-11-25 11:59:43 +0800

    re inital commit, new dir container

---
+++ CameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public Transform ship;
+
+	// Follow the player's ship smoothly
+	void Update () {
+
+		float lerpX = Mathf.Lerp(transform.position.x, ship.position.x, 0.01f);
+		float lerpY = Mathf.Lerp(transform.position.y, ship.position.y, 0.01f);
+
+		Vector3 movement = new Vector3(lerpX, lerpY, -10f);
+
+		transform.position = movement;
+	}
+}
---
+++ ShipShooter.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipShooter : MonoBehaviour {
+
+	public GameObject cannonBall;
+	public Transform cannonBallSpawn;
+
+	private bool fired = false;
+
+
+	// For Windows only
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+			fired = true;
+	}
+
+	// For Windows only
+	void FixedUpdate(){
+		if(fired){
+			fired = false;
+			GameObject rb2d = (GameObject) Instantiate(cannonBall, cannonBallSpawn.transform.position, cannonBallSpawn.rotation) as GameObject;
+			rb2d.GetComponent<Rigidbody2D>().AddRelativeForce(new Vector3(0,0.6f,0) * Time.fixedDeltaTime);
+		}
+	}
+
+	public void FireRight(){
+		GameObject rb2d = (GameObject) Instantiate(cannonBall, cannonBallSpawn.transform.position, cannonBallSpawn.rotation) as GameObject;
+		rb2d.GetComponent<Rigidbody2D>().AddRelativeForce(new Vector3(0,0.6f,0) * Time.fixedDeltaTime);
+	}
+}
