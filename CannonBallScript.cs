f9ea559f222fcb4d23f27a0a2524a637a124e7c5
Jim Paulo Ovejera <f360.jovejera@gmail.com>
2015-11-25 11:47:15 +0800

    initial commit

---
+++ CannonBallScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CannonBallScript : MonoBehaviour {
+
+	public ParticleSystem particle;
+
+	void Awake () {
+
+		// didn't collide to anything w/n 2 seconds (range), just destroy it.
+		Destroy(gameObject, 2f);
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+
+		// Instantiate an explotion particle object where it collided
+		((ParticleSystem) Instantiate(particle, transform.position, transform.rotation) as ParticleSystem).Play();
+		
+		// Destroy this cannon ball
+		Destroy(gameObject);
+	}
+	
+	void OnTriggerEnter2D(Collider2D other){
+
+		// Instantiate an explotion particle object where it collided
+		((ParticleSystem) Instantiate(particle, transform.position, transform.rotation) as ParticleSystem).Play();
+
+		// Destroy this cannon ball
+		Destroy(gameObject);
+	}
+}
---
+++ ExplosionScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplosionScript : MonoBehaviour {
+
+	void Awake () {
+
+		// Explosion particle only lasts 1 second then destroy
+		Destroy(gameObject, 1f);
+	}
+}
