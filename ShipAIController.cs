e3ec5a2b35c099d86d2b69e349bf3b0e11ef0756
Jim Paulo Ovejera <f360.jovejera@gmail.com>
2015-12-01 09:58:28 +0800

    AI ships now take damage from player

--- ShipAIController.cs
+++ ShipAIController.cs
@@ -6,23 +6,26 @@ public class ShipAIController : MonoBehaviour {
     
     private Rigidbody2D rb2d;
     private const float KNOCK_BACK_FORCE = 1.5f;
+    private ShipAIHealth aiHealth;
 
 	void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+
+        aiHealth = GetComponent<ShipAIHealth>();
 	}
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        // If this ship gets hit by other enemy's damage, then don't knock it back.
-        if(other.gameObject.tag == "EnemyDamage")
+        // If this ship gets hit by other player's damage, then knock it back and damage it.
+        if(other.gameObject.tag == "PlayerDamage")
         {
-            Destroy(other.gameObject);
-            return;
-        }
+            // Player damage hits the AI ship, apply force for knock back effect
+            rb2d.velocity = other.gameObject.transform.up * KNOCK_BACK_FORCE;
 
-        // Something hits the AI ship, apply force for knock back effect
-        rb2d.velocity = other.gameObject.transform.up * KNOCK_BACK_FORCE;
+            // Take damage from player
+            aiHealth.TakeDamage(4);
+        }
     }
 
 }
---
+++ ShipAIHealth.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ShipAIHealth : MonoBehaviour {
+
+    private int health = 100;
+
+    public Slider healthUI;
+
+    void Start()
+    {
+        healthUI.value = health;
+    }
+
+	public void TakeDamage(int damage)
+    {
+        if(health > 0)
+        {
+            health -= damage;
+
+            healthUI.value = health;
+        }
+    }
+
+    public int Health
+    {
+        get
+        {
+            return health;
+        }
+    }
+}
